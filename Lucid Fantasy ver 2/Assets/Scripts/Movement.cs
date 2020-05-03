8499a9aa81131a0c5125cf44fd92266ddc1a06c1
Jeremiah Le <jeremiah.le@snhu.edu>
2020-05-03 05:25:35 -0500

    reproject initial commit

---
+++ Lucid Fantasy ver 2/Assets/Scripts/Movement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public static bool playerFacingRight = true;
+
+    public Rigidbody2D rb;
+
+    Vector2 movement;
+
+    void Start()
+    {
+       
+    }
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * Stats.playerWalkSpeed * Time.fixedDeltaTime);
+
+        if (playerFacingRight == false && movement.x > 0 && AttackScript.shooting != true)
+        {
+            Flip();
+        }
+        else if (playerFacingRight == true && movement.x < 0 && AttackScript.shooting != true)
+        {
+            Flip();
+        }
+    }
+
+    // flipping the character's sprite
+    public void Flip()
+    {
+        playerFacingRight = !playerFacingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
+    }
+}
